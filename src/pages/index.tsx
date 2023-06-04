e1b46cc5b6ac991855734fa98049a4bf341a8836
mallowbugs <mallowbugs@gmail.com>
2023-06-04 22:30:41 +0900

    ✨ feat: 検索機能の実装
    ユーザーが情報を容易に見つけられるようにするため

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import { ComponentProps, useState } from "react";
 import { client } from "src/libs/client";
 
 export type Blog = {
@@ -11,11 +12,47 @@ export type Blog = {
 type Props = MicroCMSListResponse<Blog>;
 
 const Home: NextPage<Props> = (props) => {
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
+
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
+    e.preventDefault();
+    const q = e.currentTarget.query.value;
+    const data = await fetch("/api/search", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ q }),
+    });
+    const json: MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  const handleClick: ComponentProps<"button">["onClick"] = () => {
+    setSearch(undefined);
+  };
+
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
+
   return (
     <div>
-      <p className="text-gray-400">{`記事の総数: ${props.totalCount}件`}</p>
+      <form className="flex gap-x-3" onSubmit={handleSubmit}>
+        <input type="text" name="query" className="border border-gray-300" />
+        <button className="ml-2 rounded bg-blue-500 px-3 py-1 text-white hover:bg-blue-400">
+          検索
+        </button>
+        <button
+          type="reset"
+          className="ml-2 rounded bg-blue-500 px-3 py-1 text-white hover:bg-blue-400"
+          onClick={handleClick}
+        >
+          リセット
+        </button>
+      </form>
+      <p className="text-gray-400">
+        {`${search ? "検索結果" : "記事の総数"}: ${totalCount}件`}
+      </p>
       <ul className="mt-4 space-y-4">
-        {props.contents.map((content) => {
+        {contents.map((content) => {
           return (
             <li key={content.id}>
               <Link href={`/blog/${content.id}`}>
@@ -32,7 +69,9 @@ const Home: NextPage<Props> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await client.getList<Blog>({ endpoint: "blog" });
+  const data = await client.getList<Blog>({
+    endpoint: "blog",
+  });
   return {
     props: data,
   };
